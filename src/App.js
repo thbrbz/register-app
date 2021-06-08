9b0c073e3a8eb85de1eb2b883a7626fdb8070260
Thiago Barboza <thbrbz@gmail.com>
2021-06-08 20:15:27 -0300

    Sistema basico para realizar cadastro.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { Container, Typography } from '@material-ui/core';
+import React, { Component } from 'react';
 import './App.css';
+import RegisterForm from './components/RegisterForm/RegisterForm';
+import 'fontsource-roboto';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography>
+        <RegisterForm aoEnviar={aoEnviar} validarCPF={validarCPF} />
+      </Container>
+    );
+  }
+}
+
+function aoEnviar(dados){
+  console.log(dados);
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return{valido:false, texto:"CPF deve ter 11 digitos."}
+  }
+  return{valido:true, texto:""}
 }
 
 export default App;
